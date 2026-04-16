26421c91b627333d7a9bad2cfdc847642a0d0397
Yourname <you@email.com>
2026-04-16 23:46:02 +0400

    fix: register Specialization model in admin

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,7 @@ from .models import User, DoctorProfile, Specialization
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ("username", "email", "role", "is_stasff")
+    list_display = ("username", "email", "role", "is_staff")
     fieldsets = UserAdmin.fieldsets + (
         ("Extra Info", {"fields": ("role", "phone_number", "national_id")}),
     )
@@ -14,7 +14,13 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
 @admin.register(DoctorProfile)
 class DoctorProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "specialization", "medical_licence_number", "experience_years")
-    search_fields = ("user__username", "medical_licence_number")
+    list_display = ("user", "specialization", "medical_license_number", "experience_years")
+    search_fields = ("user__username", "medical_license_number")
