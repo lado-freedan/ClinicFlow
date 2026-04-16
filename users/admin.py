23e0360872c103014924faf224cd3804bbafd283
Yourname <you@email.com>
2026-04-16 23:36:59 +0400

    feat: register user models in admin with custom layouts

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, DoctorProfile, Specialization
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "role", "is_stasff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Extra Info", {"fields": ("role", "phone_number", "national_id")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Extra Info", {"fields": ("role", "phone_number", "national_id")}),
+    )
+
+
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "specialization", "medical_licence_number", "experience_years")
+    search_fields = ("user__username", "medical_licence_number")
